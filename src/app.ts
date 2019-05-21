9fb56a818495e4f396a04572a7ac14af1bb181e5
Thiago Silva <tsffarias@gmail.com>
2019-05-21 10:01:33 -0300

    added mvc backend architecture

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,41 @@
-import express, {Application, Request, Response, NextFunction} from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Routes } from './routes';
 
-const app: Application = express();
+class App {
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello');
-})
+    constructor() {
+        this.app = express();
+        this.config(); 
+        this.routePrv.routes(this.app); 
+        this.mongoSetup();
+    }
 
-app.listen(3000, () => console.log('Server running...'));
+    private config(): void{
+
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");         
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");         
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            next();
+        });           
+
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void{
+        const mongoose = require('mongoose');
+
+        mongoose.connect('mongodb://localhost:27017/bd_test', { useNewUrlParser: true })
+        .then(() => console.log('Successful connection...'))
+        .catch((err: any) => console.error(err));
+    }
+
+}
+
+export default new App().app;
