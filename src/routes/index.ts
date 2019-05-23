32e5a5d0826750d5cae9fde40f1c10b4bc84eb7f
tsffarias <tsffarias@gmail.com>
2019-05-23 01:22:51 -0300

    Deleted student controller + schema + routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express';
-import { StudentController } from '../controllers/studentController';
 import { UserController } from '../controllers/userController';
 
 export class Routes { 
 
-    studentController: StudentController = new StudentController();
     userController: UserController = new UserController();
 
     public routes(app: any): void { 
@@ -12,34 +10,8 @@ export class Routes {
             .get((req: Request, res: Response) => {            
                 res.status(200).send('Hello Good World!');
         }); 
-        
-        // ** Students Routes ** //
-        
-        // Get all students
-        app.route('/api/students')
-            .get(this.studentController.getStudents);
 
-        // Create a new student
-        app.route('/api/students')
-            .post(this.studentController.addNewStudent);
-
-        // get a specific student
-        app.route('/api/students/:studentId')
-            .get(this.studentController.getStudentById);
-
-        // update a specific student
-        app.route('/api/students/:studentId')
-            .put(this.studentController.updateStudent);
-        
-        // delete a specific student
-        app.route('/api/students/:studentId')
-            .delete(this.studentController.deleteStudent);
-        
-        // generate student dummy data
-        app.route('/api/dummy/students')
-            .get(this.studentController.generateDummyData);
-
-        // ** Users Routes ** //
+        // ** User Routes ** //
         
         // Get all users
         app.route('/api/users')
